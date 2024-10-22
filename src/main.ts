f641304a80271680417a757138d1b89fc175f9ad
Dongmoon Kim <ehdans04@gmail.com>
2024-10-23 00:57:37 +0900

    feat: remove https setup from code

--- src/main.ts
+++ src/main.ts
@@ -4,26 +4,15 @@ import { ValidationPipe } from '@nestjs/common';
 import { readFileSync } from 'fs';
 
 async function bootstrap() {
-  const httpsOptions = {
-    key: readFileSync('./secrets/server.key'), // 키 파일 경로
-    cert: readFileSync('./secrets/server.cert'), // 인증서 파일 경로
-  };
-  const app = await NestFactory.create(AppModule, { httpsOptions });
+  const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api');
-
-  app.use((req, res, next) => {
-    if (!req.secure) {
-      return res.redirect(`https://${req.headers.host}${req.url}`);
-    }
-    next();
-  });
 
   app.enableCors({
     origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   });
-  await app.listen(443);
+  await app.listen(8080);
 }
 bootstrap();
