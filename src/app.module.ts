fd455d1d7204599a1b176ad0cbbe0776ee657575
Dongmoon Kim <ehdans04@gmail.com>
2024-10-22 14:04:24 +0900

    feat: add dependency modules on app module

---
+++ src/app.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { BookModule } from './book/book.module';
+import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    BookModule,
+    DatabaseModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
